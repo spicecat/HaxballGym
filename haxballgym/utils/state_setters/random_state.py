478f6c7cd4a4451db4fc5524cfad0f5bc05ad821
Andy Teh <67yEGW@protonmail.ch>
2026-02-08 13:21:11 -0800

    Implement get_valid_position method in RandomState
    Add method to get valid positions for discs while avoiding overlaps.

--- haxballgym/utils/state_setters/random_state.py
+++ haxballgym/utils/state_setters/random_state.py
@@ -1,7 +1,7 @@
 import numpy as np
-from ursinaxball import Game
-
 from haxballgym.utils.state_setters import StateSetter
+from ursinaxball import Game
+from ursinaxball.objects.base.disc_object import Disc
 
 
 class RandomState(StateSetter):
@@ -9,15 +9,38 @@ class RandomState(StateSetter):
         super().__init__()
         self._rng = np.random.default_rng()
 
+    def get_valid_position(
+        self, width: int, height: int, radius: int, placed: list[Disc]
+    ):
+        max_attempts = 100
+        for _ in range(max_attempts):
+            pos = np.array(
+                [
+                    self._rng.uniform(-width + radius, width - radius),
+                    self._rng.uniform(-height + radius, height - radius),
+                ],
+                dtype=float,
+            )
+
+            for other in placed_discs:
+                if np.linalg.norm(pos - other.position) < (radius + other.radius):
+                    break
+            else:
+                return pos
+
+        return pos
+
     def reset(self, game: Game, save_recording: bool):
         game.reset(save_recording)
         width = game.stadium_game.width
         height = game.stadium_game.height
-        for disc in game.stadium_game.discs:
-            disc.position = np.array(
-                [
-                    self._rng.uniform(-width, width),
-                    self._rng.uniform(-height, height),
-                ],
-                dtype=float,
+        ball, *placed = game.stadium.discs
+
+        ball.position = self.get_valid_position(width, height, ball.radius, placed)
+        placed.append(ball)
+
+        for player in game.players:
+            player.disc.position = self.get_valid_position(
+                width, height, player.disc.radius, placed
             )
+            placed.append(player.disc)
