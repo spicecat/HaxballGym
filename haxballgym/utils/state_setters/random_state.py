0c0b8b54f7c822672358f7d13d2f62b35ce19c23
Andy Teh <67yEGW@protonmail.ch>
2026-02-08 01:04:09 -0800

    Implement RandomState StateSetter reset
    Added random number generator for disc positioning.

--- haxballgym/utils/state_setters/random_state.py
+++ haxballgym/utils/state_setters/random_state.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ursinaxball import Game
 
 from haxballgym.utils.state_setters import StateSetter
@@ -6,6 +7,17 @@ from haxballgym.utils.state_setters import StateSetter
 class RandomState(StateSetter):
     def __init__(self):
         super().__init__()
+        self._rng = np.random.default_rng()
 
-    def reset(self, game: Game, save_recording):
+    def reset(self, game: Game, save_recording: bool):
         game.reset(save_recording)
+        width = game.stadium_game.width
+        height = game.stadium_game.height
+        for disc in game.stadium_game.discs:
+            disc.position = np.array(
+                [
+                    self._rng.uniform(-width, width),
+                    self._rng.uniform(-height, height),
+                ],
+                dtype=float,
+            )
